b239871885684c2da2cb9b356d6d509b83857da2
Lam <lnhlam99@gmail.com>
2022-08-16 22:05:10 +0700

    fearture-08-fearture

--- game-doan-so.js
+++ game-doan-so.js
@@ -1,9 +1,21 @@
 function myFunction() {
-    var game = +prompt("Chơi  lại");
-    var x = Math.floor(Math.random()*100);
-    if (game == x){
-        document.writeln("Chúc mừng bạn");
-    }else
-        document.writeln("Sổ số là: " + x);
+    let x = Math.floor(Math.random() * 100);
+    var user
+    var count = 5;
+    do {
+        count--
+        if (count <= 0){
+            alert("Bạn đã hết lượt chơi")
+            break;
+        }
+        user = +prompt("Mời nhập số");
+        if (user == x)
+            alert("Bạn đã đoán đúng");
+        if (user > x)
+            alert("Số dự đoán lớn hơn số bí mật")
+        if (user < x)
+            alert("Số dự đoán nhỏ hơn số bí mật")
+    } while (x!= user)
 
 }
+
